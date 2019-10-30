0cdb1cf0c1216a7dce8a2eca9e8d656455c13743
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-10-30 15:54:16 -0500

    generating function for pausing animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,34 @@
 
-//here we difine one arrays of our gif
-const animations = ["up", "toy story 4", "frozen", "inside out"];
+//here we declare one arrays of our gif
+const animations = ["up", "frozen", "toy story", "incredibles"];
 
 function displayAnimationInfo () {
 
     const animation = $(this).attr("data-name");
     const APIKey = "PH3KRjup1KQ8EayuI8G0JSJYv4XM4jPN";
-    const queryURL = `https://api.giphy.com/v1/gifs/trending?s=${animation}&range=pg&api_key=${APIKey}`;
+    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${animation}&api_key=${APIKey}&limit=10`;
 
     $.ajax ({
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-    $("#gif-view").text(JSON.stringify(response));
+
+        $("#gif-view").empty();
+        console.log (queryURL);
+        result = response.data;
+        for(let i=0; i<result.length; i++) {
+
+            const $gif = $("<img>").attr("src", result[i].images.fixed_width.url)
+           const state = $gif.attr("data-state", "still")
+           $gif.attr("data-animate", result[i].images.fixed_width.url)
+           $gif.attr("data-still", result[i].images.fixed_width_still.url)
+            $gif.addClass("active-gif")
+             $("#gif-view").append($gif);
+            //  AnimatePause();
+        }
+     
   });
+ 
 }
 
 //here we generate function for render buttons
@@ -37,15 +52,29 @@ function renderButtons () {
         event.preventDefault();
 
         // This line grabs the input from the textbox
-        const animation = $("#gif-input").val().trim();
+        const newAnimation = $("#gif-input").val().trim();
 
         // Adding the movie from the textbox to our array
-        animations.push(animation);
-        console.log(animations);
+        animations.push(newAnimation);
+        // console.log(animations);
         renderButtons();
-      });
 
-      // Function for displaying the movie info
+
+      });
+ 
+    function AnimatePause() {
+      let state = $(this).attr("data-state");
+      if(state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+      } if(state === "animate") {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still")
+      }
+    };
+
+      // Function for displaying gifs
       $(document).on("click", ".animation", displayAnimationInfo);
+      $(document).on("click", ".active-gif", AnimatePause);
 
       renderButtons();
